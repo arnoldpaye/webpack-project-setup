1f4fc776569cc247bf54f46c50e656b0b37b7a61
Arnold Paye <arnoldpayet@gmail.com>
2017-03-23 08:37:48 -0400

    add moment dependency.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
+import moment from 'moment';
 
 function component () {
     var element = document.createElement('div');
 
     // lodash is required for the next line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    // moment is required
+    var now = moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log("Now", now);
 
     return element;
 }
